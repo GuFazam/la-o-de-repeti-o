302c87b9dd0c585f4b5fa01cba26164f60f1d9c3
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-03-01 19:35:37 -0300

    Adicionar arquivos de projeto.

---
+++ laço de repetição/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laço_de_repetição
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //listarFor.Items.Add(1);
+            //listarFor.Items.Add(2);
+            //listarFor.Items.Add(3);
+            //listarFor.Items.Add(4);
+            //listarFor.Items.Add(5);
+            //listarFor.Items.Add(6);
+            //listarFor.Items.Add(7);
+            //listarFor.Items.Add(8);
+            //listarFor.Items.Add(9);
+            //listarFor.Items.Add(10);
+        }
+    }
+}
